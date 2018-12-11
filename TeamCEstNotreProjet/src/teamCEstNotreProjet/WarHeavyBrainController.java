53d404dd74618b532babf311f600c90a45089e4c
Homère <homerebourgeois@hotmail.fr>
2018-12-11 21:01:10 +0100

    modif fsm heavy et rocket

--- TeamCEstNotreProjet/src/teamCEstNotreProjet/WarHeavyBrainController.java
+++ TeamCEstNotreProjet/src/teamCEstNotreProjet/WarHeavyBrainController.java
@@ -1,42 +1,201 @@
 package teamCEstNotreProjet;
 
+import edu.warbot.agents.agents.WarExplorer;
+import edu.warbot.agents.agents.WarRocketLauncher;
 import edu.warbot.agents.enums.WarAgentType;
 import edu.warbot.agents.percepts.WarAgentPercept;
+import edu.warbot.agents.projectiles.WarBomb;
+import edu.warbot.agents.projectiles.WarShell;
+import edu.warbot.brains.WarBrain;
 import edu.warbot.brains.brains.WarHeavyBrain;
 import edu.warbot.brains.brains.WarRocketLauncherBrain;
+import edu.warbot.communications.WarMessage;
+import edu.warbot.tools.geometry.PolarCoordinates;
 
 public abstract class WarHeavyBrainController extends  WarHeavyBrain {
 
 
+
+    
+    WTask ctask;
+    Sorted_Percepts sp;
+    boolean hasTarget;
+    int cptrTarget;
+    int nbTickBeforeAbandon;
+    PolarCoordinates targetDirection;
+    double shellDistance=WarShell.AUTONOMY*WarShell.SPEED;
+
     public WarHeavyBrainController() {
         super();
+        ctask=MoveToExplorer;
+        hasTarget =false;
+        cptrTarget=0;
+        nbTickBeforeAbandon=20;
     }
 
     @Override
     public String action() {
 
-        for (WarAgentPercept wp : getPerceptsEnemies()) {
 
-            if (wp.getType().equals(WarAgentType.WarBase) || 
-            		wp.getType().equals(WarAgentType.WarRocketLauncher) || 
-            		wp.getType().equals(WarAgentType.WarHeavy)  ) {
 
-                setHeading(wp.getAngle());
-
-                this.setDebugString("Attaque!");
-                if (isReloaded())
-                    return ACTION_FIRE;
-                else if (isReloading())
-                    return ACTION_IDLE;
-                else
-                    return ACTION_RELOAD;
+        String toReturn = ctask.exec(this);   // le run de la FSM
+        
+        if(toReturn == null){
+            if (isBlocked())
+                setRandomHeading();
+            return WarHeavyBrainController.ACTION_MOVE;
+        } else {
+            return toReturn;
+        }
+    }
+    
+    static WTask ShootTarget = new WTask(){
+        String exec(WarBrain bc)
+        {
+            WarHeavyBrainController me = (WarHeavyBrainController) bc;
+    		
+            WarMessage message= me.getMessageAboutEnemiesInRange();
+            if(message==null)
+            {
+	            me.cptrTarget--; 
+            }
+            else
+            {
+            	me.hasTarget=true;
+    			me.cptrTarget=me.nbTickBeforeAbandon;
+                me.targetDirection = me.getTargetedAgentPosition(message.getAngle(), message.getDistance(), Double.parseDouble(message.getContent()[1]),Double.parseDouble(message.getContent()[0]));
+            }
+            me.setHeading(me.targetDirection.getAngle());
+            if(me.isReloaded()) //si il est rechargé, il tire
+            {
+                return WarHeavyBrainController.ACTION_FIRE;
+            }
+            if(me.cptrTarget<=0)
+            {
+            	me.ctask = MoveToExplorer;
+            }
+            if(!me.isReloaded())
+            {   
+            	     		           
+            	return WarHeavyBrainController.ACTION_RELOAD;          
+            }
+            return WarHeavyBrainController.ACTION_MOVE;     
+        }
+        
+    };
+    
+    static WTask MoveToTarget = new WTask(){
+        String exec(WarBrain bc)
+        {
+        	
+        	WarHeavyBrainController me = (WarHeavyBrainController) bc;
+           
+        	WarMessage message= me.getMessageAboutEnemiesInRange();    //detecte si des ennemis sont a portée de rocket
+            if(message!=null)
+            {
+        		me.hasTarget=true;
+        		me.cptrTarget=me.nbTickBeforeAbandon;
+            	me.setDebugString(message.getContent().toString());
+                me.ctask=ShootTarget;
+        		me.hasTarget=true;
+            }
+            else if(me.getMessageAboutClosestEnemy()!=-1)
+        	{
+        		int numMessage = me.getMessageAboutClosestEnemy(); // sinon  un ennemi a été repéré mais ne peut pas etre touché?
+        		WarMessage messageClosestEnemy = me.getMessages().get(numMessage);
+        		PolarCoordinates blub=me.getTargetedAgentPosition(messageClosestEnemy.getAngle(), messageClosestEnemy.getDistance(), Double.parseDouble(messageClosestEnemy.getContent()[1]),Double.parseDouble(messageClosestEnemy.getContent()[0]));
+        		me.hasTarget=true;
+        		me.cptrTarget=me.nbTickBeforeAbandon;
+        		double targetAngle= blub.getAngle();
+        		me.setHeading(targetAngle);
+        		if (me.isBlocked())
+                me.setRandomHeading();
+            	return WarHeavyBrainController.ACTION_MOVE;
+        	}
+        	else if(me.cptrTarget<=0)
+        	{
+        		me.ctask=MoveToExplorer;
+        	}
+            me.cptrTarget--;
+        	return WarHeavyBrainController.ACTION_MOVE;
+            //double targetDistance =Double.parseDouble(message.getContent()[0]);   
+            
+        }
+    };
+    
+    static WTask MoveToExplorer = new WTask(){
+        String exec(WarBrain bc)
+        {
+        	WarHeavyBrainController me = (WarHeavyBrainController) bc;
+            
+            //}
+            WarMessage message= me.getMessageAboutEnemiesInRange();    //detecte si des ennemis sont a portée de rocket
+            int numMessage = me.getMessageAboutClosestEnemy(); // sinon  un ennemi a été repéré mais ne peut pas etre touché?
+            if(message!=null)
+            {
+            	me.setDebugString(message.getContent().toString());
+                me.ctask=ShootTarget;
+                me.setHeading(Double.parseDouble(message.getContent()[1]));
+                return WarHeavyBrainController.ACTION_FIRE;
+            }
+            else if(numMessage!=-1) // c'est oui
+            {
+                me.ctask=MoveToTarget;
+            }
+            if (me.isBlocked())
+                me.setRandomHeading();
+            return WarHeavyBrainController.ACTION_MOVE;
+        }
+    };
+    
+    
+    private WarMessage getMessageAboutEnemiesInRange() {
+        for (WarMessage m : getMessages())
+        {
+            if(m.getMessage().equals(ContenuMessage.TargetSpotted.toString()))
+            {
+            	setDebugString("coucou");
+                if(isTargetInRange(m))
+                {
+                    return m ;
+                }
             }
         }
-
-        if (isBlocked())
-            setRandomHeading();
-
-        return ACTION_MOVE;
+        return null;
     }
+    
+    protected int getMessageAboutClosestEnemy() {
+        double previousDistance =9999;
+        int numMessage=-1;
+
+        for (int i =0;i < this.getMessages().size();i++)
+        {
+
+            if(this.getMessages().get(i).getMessage().equals(ContenuMessage.TargetSpotted.toString()))
+            {   
+            	setDebugString("hola");
+                PolarCoordinates blub=getTargetedAgentPosition(getMessages().get(i).getAngle(), getMessages().get(i).getDistance(), Double.parseDouble(getMessages().get(i).getContent()[1]),Double.parseDouble(getMessages().get(i).getContent()[0]));
+
+                if(Math.abs(blub.getDistance()-150)<previousDistance)
+                {
+                    previousDistance=blub.getDistance();
+                    numMessage=i;
+                }
+                
+            }
+        }
+        return numMessage;
+    }
+    
+    private Boolean isTargetInRange(WarMessage m)
+    {
+    	PolarCoordinates blub= getTargetedAgentPosition(m.getAngle(), m.getDistance(), Double.parseDouble(m.getContent()[1]),Double.parseDouble(m.getContent()[0]));  
+        if((blub.getDistance()<shellDistance))
+        {
+            return true;
+        }
+        return false;
+    }
+
 
 }
--- TeamCEstNotreProjet/src/teamCEstNotreProjet/WarRocketLauncherBrainController.java
+++ TeamCEstNotreProjet/src/teamCEstNotreProjet/WarRocketLauncherBrainController.java
@@ -17,7 +17,10 @@ public abstract class WarRocketLauncherBrainController extends WarRocketLauncher
 
     WTask ctask;
     Sorted_Percepts sp;
-    String target;
+    boolean hasTarget;
+    int cptrTarget;
+    int nbTickBeforeAbandon;
+    PolarCoordinates targetDirection;
     double rocketDistance=WarBomb.AUTONOMY*WarBomb.SPEED;
 
     public WarRocketLauncherBrainController() {
@@ -28,113 +31,125 @@ public abstract class WarRocketLauncherBrainController extends WarRocketLauncher
 
     @Override
     public String action() {
-        ctask=null;
-            WarMessage message= getMessageAboutEnemiesInRange();    //detecte si des ennemis sont a portée de rocket
-            int numMessage = getMessageAboutClosestEnemy(); // sinon  un ennemi a été repéré mais ne peut pas etre touché?
-            if(message!=null)
-            {
-            	setDebugString(message.getContent().toString());
-                ctask=ShootTarget;
-            }
-            else if(numMessage!=-1) // c'est oui
-            {
-                ctask=MoveToTarget;
-            }
-            else
-            {
-            	ctask=MoveToExplorer;
-            }
 
-            String toReturn = ctask.exec(this);   // le run de la FSM
-            
-            if(toReturn == null){
-                if (isBlocked())
-                    setRandomHeading();
-                return WarExplorer.ACTION_MOVE;
-            } else {
-                return toReturn;
-            
+
+
+        String toReturn = ctask.exec(this);   // le run de la FSM
+        
+        if(toReturn == null){
+            if (isBlocked())
+                setRandomHeading();
+            return WarRocketLauncherBrainController.ACTION_MOVE;
+        } else {
+            return toReturn;
         }
     }
     
     static WTask ShootTarget = new WTask(){
         String exec(WarBrain bc)
         {
-            WarRocketLauncherBrainController me = (WarRocketLauncherBrainController) bc;
-            WarMessage message= me.getMessageAboutEnemiesInRange(); //detecte si des ennemis sont a portée de rocket
-//          if(message!=null) //si oui
-//          {
-            PolarCoordinates target = me.getTargetedAgentPosition(message.getAngle(), message.getDistance(), Double.parseDouble(message.getContent()[1]),Double.parseDouble(message.getContent()[0]));
-            me.setHeading(target.getAngle());
-            me.setTargetDistance(target.getDistance());
+        	WarRocketLauncherBrainController me = (WarRocketLauncherBrainController) bc;
+    		
+            WarMessage message= me.getMessageAboutEnemiesInRange();
+            if(message==null)
+            {
+	            me.cptrTarget--; 
+            }
+            else
+            {
+            	me.hasTarget=true;
+    			me.cptrTarget=me.nbTickBeforeAbandon;
+                me.targetDirection = me.getTargetedAgentPosition(message.getAngle(), message.getDistance(), Double.parseDouble(message.getContent()[1]),Double.parseDouble(message.getContent()[0]));
+            }
+            me.setHeading(me.targetDirection.getAngle());
+            me.setTargetDistance(me.targetDirection.getDistance());
+
             if(me.isReloaded()) //si il est rechargé, il tire
             {
-                return WarRocketLauncher.ACTION_FIRE;
+                return WarRocketLauncherBrainController.ACTION_FIRE;
             }
-            return WarRocketLauncher.ACTION_RELOAD;
-//          }
-//          else
-//          {
-//              int numMessage = me.getMessageAboutClosestEnemy(); // sinon  un ennemi a été repéré mais ne peut pas etre touché?
-//              if(numMessage!=-1) // c'est oui
-//              {
-//                  message = me.getMessages().get(numMessage);
-//                  double targetDistance =Double.parseDouble(message.getContent()[0]);
-//                  double targetAngle= Double.parseDouble(message.getContent()[1]);
-//                  if(targetDistance<0) // il doit reculer
-//                  {
-//                      me.setHeading(targetAngle+180);
-//                      return WarRocketLauncher.ACTION_MOVE;
-//                  }
-//                  else // il doit avancer
-//                  {
-//                      me.setHeading(targetAngle);
-//                      return WarRocketLauncher.ACTION_MOVE;
-//                  }
-//              }
-//              else  // c'est non
-//              {
-//                  
-//              }
-//          }
-//          return WarRocketLauncher.ACTION_IDLE;
+            if(me.cptrTarget<=0)
+            {
+            	me.ctask = MoveToExplorer;
+            }
+            if(!me.isReloaded())
+            {   
+            	     		           
+            	return WarRocketLauncherBrainController.ACTION_RELOAD;          
+            }
+            return WarRocketLauncherBrainController.ACTION_MOVE;     
         }
+        
     };
     
     static WTask MoveToTarget = new WTask(){
         String exec(WarBrain bc)
         {
         	
-            WarRocketLauncherBrainController me = (WarRocketLauncherBrainController) bc;
-            int numMessage = me.getMessageAboutClosestEnemy(); // sinon  un ennemi a été repéré mais ne peut pas etre touché?
-            me.setTargetDistance(WarBomb.AUTONOMY*WarBomb.SPEED);
-            WarMessage message = me.getMessages().get(numMessage);
-            //double targetDistance =Double.parseDouble(message.getContent()[0]);
-            
-            PolarCoordinates blub=me.getTargetedAgentPosition(message.getAngle(), message.getDistance(), Double.parseDouble(message.getContent()[1]),Double.parseDouble(message.getContent()[0]));
-            double targetAngle= blub.getAngle();
-            me.setHeading(targetAngle);
-            if (me.isBlocked())
+        	WarRocketLauncherBrainController me = (WarRocketLauncherBrainController) bc;
+           
+        	WarMessage message= me.getMessageAboutEnemiesInRange();    //detecte si des ennemis sont a portée de rocket
+            if(message!=null)
+            {
+        		me.hasTarget=true;
+        		me.cptrTarget=me.nbTickBeforeAbandon;
+            	me.setDebugString(message.getContent().toString());
+                me.ctask=ShootTarget;
+        		me.hasTarget=true;
+            }
+            else if(me.getMessageAboutClosestEnemy()!=-1)
+        	{
+        		int numMessage = me.getMessageAboutClosestEnemy(); // sinon  un ennemi a été repéré mais ne peut pas etre touché?
+        		WarMessage messageClosestEnemy = me.getMessages().get(numMessage);
+        		PolarCoordinates blub=me.getTargetedAgentPosition(messageClosestEnemy.getAngle(), messageClosestEnemy.getDistance(), Double.parseDouble(messageClosestEnemy.getContent()[1]),Double.parseDouble(messageClosestEnemy.getContent()[0]));
+        		me.hasTarget=true;
+        		me.cptrTarget=me.nbTickBeforeAbandon;
+        		double targetAngle= blub.getAngle();
+        		me.setHeading(targetAngle);
+                me.setTargetDistance(me.rocketDistance);
+        		if (me.isBlocked())
                 me.setRandomHeading();
-            return WarRocketLauncher.ACTION_MOVE;
+            	return WarRocketLauncherBrainController.ACTION_MOVE;
+        	}
+        	else if(me.cptrTarget<=0)
+        	{
+        		me.ctask=MoveToExplorer;
+        	}
+            me.cptrTarget--;
+        	return WarRocketLauncherBrainController.ACTION_MOVE;
+            //double targetDistance =Double.parseDouble(message.getContent()[0]);   
+            
         }
     };
     
     static WTask MoveToExplorer = new WTask(){
         String exec(WarBrain bc)
         {
-            WarRocketLauncherBrainController me = (WarRocketLauncherBrainController) bc;
+        	WarRocketLauncherBrainController me = (WarRocketLauncherBrainController) bc;
             
             //}
+            WarMessage message= me.getMessageAboutEnemiesInRange();    //detecte si des ennemis sont a portée de rocket
+            int numMessage = me.getMessageAboutClosestEnemy(); // sinon  un ennemi a été repéré mais ne peut pas etre touché?
+            if(message!=null)
+            {
+            	me.setDebugString(message.getContent().toString());
+                me.ctask=ShootTarget;
+                me.setHeading(Double.parseDouble(message.getContent()[1]));
+                me.setTargetDistance(me.rocketDistance);
+                return WarRocketLauncherBrainController.ACTION_FIRE;
+            }
+            else if(numMessage!=-1) // c'est oui
+            {
+                me.ctask=MoveToTarget;
+            }
             if (me.isBlocked())
                 me.setRandomHeading();
-            return WarRocketLauncher.ACTION_MOVE;
-            //return WarRocketLauncher.ACTION_IDLE;
+            return WarRocketLauncherBrainController.ACTION_MOVE;
         }
     };
     
     
-    private WarMessage getMessageAboutEnemiesInRange() {
+    protected WarMessage getMessageAboutEnemiesInRange() {
         for (WarMessage m : getMessages())
         {
             if(m.getMessage().equals(ContenuMessage.TargetSpotted.toString()))
@@ -172,8 +187,10 @@ public abstract class WarRocketLauncherBrainController extends WarRocketLauncher
     
     private Boolean isTargetInRange(WarMessage m)
     {
-        double messageDistance = Double.parseDouble(m.getContent()[0]);
-        if((messageDistance<rocketDistance+WarBomb.EXPLOSION_RADIUS))
+        
+        PolarCoordinates blub= getTargetedAgentPosition(m.getAngle(), m.getDistance(), Double.parseDouble(m.getContent()[1]),Double.parseDouble(m.getContent()[0]));
+        
+        if((blub.getDistance()<rocketDistance+WarBomb.EXPLOSION_RADIUS))
         {
             return true;
         }
